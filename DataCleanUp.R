361711d130538f02fbe03847e67a7189307a86df
Elizabeth <earangor@unal.edu.co>
2021-03-31 23:09:20 -0400

    Reading Non_Violent Crime

--- DataCleanUp.R
+++ DataCleanUp.R
@@ -1,22 +1,36 @@
 # Load Required Library
 
-library('sf')
+library(sf)
 library(tidyverse)
-library(plotly)
-library(cartogram)
-library(geog4ga3)
-library(spdep)
-library(plotly)
+#library(plotly)
+#library(cartogram)
+#library(geog4ga3)
+#library(spdep)
+#library(plotly)
 
-# Load .csv file 
 
-updatedData <- ## Lucy's file + existing data 
-updatedData$SCODE_NAME <- as.factor(Variables$SCODE_NAME)
+# Merge .csv files
+
+updatedData <- merge(x = Demographic, y = CRIME, by = "SCODE_NAME")
   
 # Load shapefile
 
-ward44 <- read.sf("C:/Users/sooah/stats/WardTotonto.shp")
+wards44 <- read_sf("C:/Users/Elizabeth/Documents/GitHub/ES4GA3-Group-5/WardToronto.shp")
 
 # Merge wards shapefile to updatedData
 
 Toronto_Data <- merge(x = wards44, y = updatedData, by = "SCODE_NAME")
+
+# Plot Violent Crime
+
+ggplot(Toronto_Data) + 
+  geom_sf(aes(fill = cut_number(NONVIOLENTC, 5)), color = NA, size = 0.1) +
+  scale_fill_brewer(palette = "YlOrRd") +
+  coord_sf() +
+  labs(fill = "Non-Violent Crime")
+
+ggplot(Toronto_Data) + 
+  geom_sf(aes(fill = cut_number(P_NONVIOLENTC, 5)), color = NA, size = 0.1) +
+  scale_fill_brewer(palette = "YlOrRd") +
+  coord_sf() +
+  labs(fill = "Percentage Non-Violent Crime")
